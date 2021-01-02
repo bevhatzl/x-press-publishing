42276ce99292d36a7541a00f94be303a2b40f1cb
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2021-01-02 19:52:55 +1100

    get handler for all series

--- api/series.js
+++ api/series.js
@@ -5,4 +5,16 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 
+seriesRouter.get('/', (req, res, next) => {
+
+    db.all("SELECT * FROM Series", (error, series) => {
+        if (error) {
+            next(error);
+        } else {
+            res.status(200).json({ series: series });
+        }
+    });
+
+});
+
 module.exports = seriesRouter;
