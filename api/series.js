7cec82134a0f0daea22c244e27916b24d104135a
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2021-01-02 20:53:28 +1100

    add router param handler for seriesId

--- api/series.js
+++ api/series.js
@@ -4,6 +4,21 @@ const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+// Router param for handling the seriesID parameter.
+seriesRouter.param('seriesId', (req, res, next, seriesId) => {
+    const sql = 'SELECT * FROM Series WHERE Series.id = $seriesId';
+    const values = { $seriesId: seriesId };
+    db.get(sql, values, (error, series) => {
+        if (error) {
+            next(error);
+        } else if (series) {
+            req.series = series;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    });
+});
 
 seriesRouter.get('/', (req, res, next) => {
 
